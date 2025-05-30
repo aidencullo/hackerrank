be10a9c3dacbcc2521ca277b1792e517c35cf236
Aiden Cullo <aidencullo@gmail.com>
2025-05-30 14:56:58 -0400

    doesn't timeout
    1 time/space

--- save-the-prisoner/solution.py
+++ save-the-prisoner/solution.py
@@ -1,6 +1,8 @@
 def saveThePrisoner(n, m, s):
     # Write your code here
     final_seat = (s + (m - 1))
-    while final_seat > n:
-        final_seat -= n
+    if final_seat % n == 0:
+        final_seat = n
+    else:
+        final_seat %= n
     return final_seat
