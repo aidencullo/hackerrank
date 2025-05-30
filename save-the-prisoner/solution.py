c5ac62e2883f08a1184c3609665d0e3a50260145
Aiden Cullo <aidencullo@gmail.com>
2025-05-30 15:26:59 -0400

    remove control statement and decision tree

--- save-the-prisoner/solution.py
+++ save-the-prisoner/solution.py
@@ -1,8 +1,3 @@
 def saveThePrisoner(n, m, s):
-    # Write your code here
     final_seat = (s + (m - 1))
-    if final_seat % n == 0:
-        final_seat = n
-    else:
-        final_seat %= n
-    return final_seat
+    return ((final_seat - 1) % n) + 1
