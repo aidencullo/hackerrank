f58785cf0fee0b77c61894b51383bf5461cd7379
aiden cullo <aidencullo@gmail.com>
2025-08-07 14:06:04 -0400

    Add number-of-items solution - passed most test cases, failed on time for a few edge cases

---
+++ number-of-items/solution.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'numberOfItems' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER_ARRAY startIndices
+#  3. INTEGER_ARRAY endIndices
+#
+
+def countItems(s, start, end):
+    return s[start - 1: end].strip('*').count('*')
+
+def numberOfItems(s, startIndices, endIndices):
+    # Write your code here
+    n = len(startIndices)
+    result = []
+    for i in range(n):
+        result.append(countItems(s, startIndices[i], endIndices[i]))
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    startIndices_count = int(input().strip())
+
+    startIndices = []
+
+    for _ in range(startIndices_count):
+        startIndices_item = int(input().strip())
+        startIndices.append(startIndices_item)
+
+    endIndices_count = int(input().strip())
+
+    endIndices = []
+
+    for _ in range(endIndices_count):
+        endIndices_item = int(input().strip())
+        endIndices.append(endIndices_item)
+
+    result = numberOfItems(s, startIndices, endIndices)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
