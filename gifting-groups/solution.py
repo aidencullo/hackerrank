d117040aaa54d4881dc95edf269f02372c3f677e
aidencullo <aidencullo@gmail.com>
2025-08-11 13:49:38 -0400

    count groups boilerplate

---
+++ gifting-groups/solution.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the function below.
+#
+
+def solve():
+    # Write your code here
+    pass
+
+if __name__ == '__main__':
+    # Read input and call solve function
+    pass
