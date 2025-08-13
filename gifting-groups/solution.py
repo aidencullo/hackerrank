0def3d534d741afb2e864790de98d3f1068ddc83
aidencullo <aidencullo@gmail.com>
2025-08-13 12:09:13 -0400

    Update gifting-groups solution

--- gifting-groups/solution.py
+++ gifting-groups/solution.py
@@ -6,14 +6,32 @@ import random
 import re
 import sys
 
+
+
 #
-# Complete the function below.
+# Complete the 'countGroups' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING_ARRAY related as parameter.
 #
 
-def solve():
+def countGroups(related):
     # Write your code here
     pass
 
 if __name__ == '__main__':
-    # Read input and call solve function
-    pass
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    related_count = int(input().strip())
+
+    related = []
+
+    for _ in range(related_count):
+        related_item = input()
+        related.append(related_item)
+
+    result = countGroups(related)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
