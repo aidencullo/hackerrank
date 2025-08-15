53aab4b0fdc4d1261192232a21b8d342de99d329
aidencullo <aidencullo@gmail.com>
2025-08-15 15:51:44 -0400

    Add best effort solution for enhanced-sequence (ran out of time)

--- enhanced-sequence/solution.py
+++ enhanced-sequence/solution.py
@@ -19,7 +19,30 @@ import sys
 
 def calculateEnhancedSequences(userKey, systemKey):
     # Write your code here
-    pass
+    def explore(user_index, system_index):
+        nonlocal total
+        if system_index == m and user_index == n:
+            return
+        if system_index == m:
+            total += 2 ** ((n - 1) - (user_index - 1))
+            return
+        if user_index == n:
+            return
+
+        if userKey[user_index] > systemKey[system_index]:
+            total += 2 ** ((n - 1) - user_index)
+        elif userKey[user_index] < systemKey[system_index]:
+            explore(user_index + 1, system_index)
+        else:
+            explore(user_index + 1, system_index)
+            explore(user_index + 1, system_index + 1)
+
+
+    n = len(userKey)
+    m = len(systemKey)
+    total = 0
+    explore(0, 0)
+    return total  % (10^9 + 7)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
